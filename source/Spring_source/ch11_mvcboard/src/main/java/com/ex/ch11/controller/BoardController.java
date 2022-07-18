7c3e76c330161cf09bff32123671e1fa5457ea3e
user <wkdrngla789@naver.com>
2022-07-18 23:52:57 +0900

    0718

---
+++ source/Spring_source/ch11_mvcboard/src/main/java/com/ex/ch11/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.ex.ch11.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ex.ch11.service.BListService;
+import com.ex.ch11.service.Service;
+
+@Controller
+@RequestMapping("mvcboard") // 공통요청경로
+public class BoardController {
+	private Service service;
+	@RequestMapping(value="list", method=RequestMethod.GET)
+	public String list(String pageNum, Model model) {
+		model.addAttribute("pageNum", pageNum);
+		service = new BListService();
+		service.execute(model);
+		return "mvcmodel/list";
+	}
+}
---
+++ source/Spring_source/ch11_mvcboard/src/main/java/com/ex/ch11/service/Service.java
@@ -0,0 +1,7 @@
+package com.ex.ch11.service;
+
+import org.springframework.ui.Model;
+
+public interface Service {
+	public void execute(Model model);
+}
