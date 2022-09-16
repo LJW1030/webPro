80cf0f084886b31f7cb3c2afa1cef5b0604825de
user <wkdrngla789@naver.com>
2022-09-16 23:21:42 +0900

    0916

--- source/JAVA_source/lastEx/src/com/lec/ex4_supermarket/KungFu.java
+++ source/JAVA_source/lastEx/src/com/lec/ex4_supermarket/KungFu.java
@@ -2,10 +2,6 @@ package com.lec.ex4_supermarket;
 
 public class KungFu {
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
-		int c = --a;
-		int d = b/c;
-		System.out.println(d);
+		System.out.println("Hello World");
 	}
 }
